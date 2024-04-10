2feecf2763f0830eb7edb7a8e2150ffdb87080bf
Raiko <raiko.tohk@ametikool.ee>
2024-04-10 11:27:31 +0300

    deploy2

--- deploy.php
+++ deploy.php
@@ -16,7 +16,30 @@ host('ta22tohk.itmajakas.ee')
     ->set('deploy_path', '~/domeenid/www.ta22tohk.itmajakas.ee/hajus')
     ->set('branch', 'master');
 
-set('repository', 'https://gitlab.com/RalfHei/hajusrakendus.git');
+set('repository', 'git@github.com:Raikotohk1/hajusrakendus.git');
+
+task('opcache:clear', function () {
+    run('killall php82-cgi || true');
+})->desc('Clear opcache');
+
+task('build:node', function () {
+    cd('{{release_path}}');
+    run('npm i');
+    run('npx vite build');
+    run('rm -rf node_modules');
+});
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'build:node',
+    'deploy:publish',
+    'opcache:clear',
+    'artisan:cache:clear'
+]);
 
 add('shared_files', []);
 add('shared_dirs', []);
